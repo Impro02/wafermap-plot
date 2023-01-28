9bf52ec14bfc9ce851c01c8c372b58a29353a75e
Maxime MARTIN <maxime.martin@st.com>
2023-01-28 14:52:01 +0100

    format: remove old function and add type

--- wafermap_plot/libs/color.py
+++ wafermap_plot/libs/color.py
@@ -1,23 +1,15 @@
 import random
-from typing import List
-
+from typing import List, Tuple
 
 from sklearn.metrics.pairwise import euclidean_distances
 
-import numpy as np
+
+def random_rgb_color() -> Tuple[int, int, int]:
+    return (random.randint(0, 255) for _ in range(3))
 
 
-def random_rgb_color():
-    # return np.random.randint(0, 255, size=(3,))
-    return [random.randint(0, 255) for _ in range(3)]
-
-
-def convert_rgb_hex(rgb):
+def convert_rgb_hex(rgb: Tuple[int, int, int]) -> str:
     return "#{:02x}{:02x}{:02x}".format(*rgb)
-
-
-def delta_E_CIE1976(Lab1, Lab2):
-    return np.linalg.norm(np.array(Lab1) - np.array(Lab2))
 
 
 def generate_distinct_colors(nbr_colors: int) -> List[str]:
